426d7c2561e07ef72402af6ee3359f123cf92ca1
redtrib3 <anirudhkp2004@gmail.com>
2025-08-03 18:23:57 +0530

    New: YAML validation script

---
+++ scripts/validate_yaml.py
@@ -0,0 +1,22 @@
+'''
+Validate the yaml files
+'''
+import os
+import yaml
+
+error_log = {}
+
+for filename in os.listdir():
+    if filename.endswith(".yaml") or filename.endswith(".yml"):
+        try:
+            with open(filename, "r", encoding="utf-8") as f:
+                yaml.safe_load(f)
+        except yaml.YAMLError as e:
+            error_log[filename] = str(e).splitlines()[0]  # just the first line of the error
+
+if error_log:
+    print("Files with YAML errors:\n")
+    for file, err in error_log.items():
+        print(f"{file}: {err}")
+else:
+    print("All YAML files parsed successfully.")
